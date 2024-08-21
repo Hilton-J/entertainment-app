8328878833228b05bfe03b65c4080cd0a08b3656
Hilton-J <140448818+Hilton-J@users.noreply.github.com>
2024-08-21 14:47:31 +0200

    NavBar Cleanup

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,7 +13,9 @@ const NavBar = () => {
     <header className="bg-slate-900">
       <nav className="container mx-auto flex justify-between items-center p-4">
         <NavLink to={"/"} className='text-xl w-fit sm:text-2xl font-bold text-blue-600 hover:text-blue-500'>Steam-Trends</NavLink>
+
         <div className={`md:static absolute bg-slate-900 md:min-h-fit min-h-[30vh] left-0 ${isMenuOpen ? 'top-[55px]' : 'top-[100%]'} md:w-auto w-full flex items-center justify-center px-5`}>
+
           <ul className="flex md:flex-row flex-col gap-4 lg:gap-8 items-center">
             <li className="m-0"><NavLink to={"/"}
               className={({ isActive }) => `${linkClass({ isActive })} hover:text-blue-500`}>HOME</NavLink></li>
@@ -21,10 +23,16 @@ const NavBar = () => {
             <li><NavLink to={"/popular"} className={({ isActive }) => `${linkClass({ isActive })} hover:text-blue-500`}>POPULAR</NavLink></li>
             <li><NavLink to={"/tv-shows"} className={({ isActive }) => `${linkClass({ isActive })} hover:text-blue-500`}>TV SHOWS</NavLink></li>
           </ul>
+
         </div>
+
         <div className="flex items-center gap-4">
+
           <button className="bg-blue-600 hover:bg-blue-500 text-white px-4 py-2 rounded-lg block text-sm md:text-base">SUBSCRIBE</button>
-          <a onClick={toggleMenu} className="text-3xl cursor-pointer text-gray-600 md:hidden"> {isMenuOpen ? <IoCloseOutline /> : <IoMenuOutline />}</a>
+          <a onClick={toggleMenu} className="text-3xl cursor-pointer text-gray-600 md:hidden">
+            {isMenuOpen ? <IoCloseOutline /> : <IoMenuOutline />}
+          </a>
+
         </div>
       </nav>
     </header>
