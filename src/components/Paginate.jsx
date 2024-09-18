05cdec6a35b4b2e47f6f6d308a80033689ec5298
Hilton-J <140448818+Hilton-J@users.noreply.github.com>
2024-09-18 12:43:06 +0200

    uo

--- src/components/Paginate.jsx
+++ src/components/Paginate.jsx
@@ -36,7 +36,6 @@ const Paginate = ({ setCurrentPage, pageCount = 50 }) => {
 
 Paginate.propTypes = {
   setCurrentPage: PropTypes.func,
-  setCurrentPage: PropTypes.func,
   pageCount: PropTypes.number
 }
 
