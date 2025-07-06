b64a80a6f38c868d9d4a86a87edc3407c08184f0
Hilton-J <140448818+Hilton-J@users.noreply.github.com>
2025-07-06 17:24:14 +0200

    vercel configuration

--- server/index.mjs
+++ server/index.mjs
@@ -16,6 +16,10 @@ const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Hello, Vercel!')
+})
+
 // Route to handle TMDB API requests
 app.use('/api/tvshows', tvRouter);
 app.use('/api/movies', movieRouter);
@@ -26,3 +30,5 @@ app.use('/api/trending', trendingRouter);
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+export default app;
