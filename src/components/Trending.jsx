1fdcd0551a2dfcab10d8bd6ffe737f1fa690abf5
Hilton-J <140448818+Hilton-J@users.noreply.github.com>
2025-07-06 19:24:42 +0200

    Added an bckend environment variable

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -12,7 +12,7 @@ const Trending = () => {
 
   useEffect(() => {
     const fetchTrending = async () => {
-      const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/trending`)
+      const { data } = await axios.get(`${import.meta.env.EXPRESS_API_URL}/trending`)
       data.total_pages < 50 ? setPageCount(data.total_pages) : setPageCount(50)
       setTrending(data.results)
 
